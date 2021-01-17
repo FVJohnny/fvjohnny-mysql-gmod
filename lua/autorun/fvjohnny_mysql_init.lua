8ea5f6b04fda175116f09fce77f009cee702c274
FVJohnny <beox12@gmail.com>
2021-01-17 15:52:45 +0100

    Better console prints when the module is loaded

--- lua/autorun/fvjohnny_mysql_init.lua
+++ lua/autorun/fvjohnny_mysql_init.lua
@@ -26,7 +26,6 @@ function FVJOHNNY_MYSQL:LoadFile(path, serverOnly, clientOnly)
     if clientOnly and SERVER then return end
     if serverOnly and CLIENT then return end
 
-
     include(path)
     print("[FVJOHNNY_MYSQL] Loaded file: "..path) 
 end
@@ -38,12 +37,27 @@ function FVJOHNNY_MYSQL:LoadFiles()
     self:LoadFile("fvjohnny_mysql/db/tables.lua", true, false)
     self:LoadFile("fvjohnny_mysql/db/query.lua", true, false)
     self:LoadFile("fvjohnny_mysql/server.lua", true, false)
-    //self:LoadFile("fvjohnny_mysql/db/databases.lua", true, false)
     self:LoadFile("fvjohnny_mysql/client.lua", false, true)
 end
 
 function FVJOHNNY_MYSQL:Credits()
-    print("[FVJOHNNY_MYSQL] Made with love by FVJohnny http://steamcommunity.com/id/FVJohnny/") 
+    print([[
+        
+    ███████╗██╗░░░██╗░░░░░██╗░█████╗░██╗░░██╗███╗░░██╗███╗░░██╗██╗░░░██╗  ███╗░░░███╗██╗░░░██╗░██████╗░██████╗░██╗
+    ██╔════╝██║░░░██║░░░░░██║██╔══██╗██║░░██║████╗░██║████╗░██║╚██╗░██╔╝  ████╗░████║╚██╗░██╔╝██╔════╝██╔═══██╗██║
+    █████╗░░╚██╗░██╔╝░░░░░██║██║░░██║███████║██╔██╗██║██╔██╗██║░╚████╔╝░  ██╔████╔██║░╚████╔╝░╚█████╗░██║██╗██║██║
+    ██╔══╝░░░╚████╔╝░██╗░░██║██║░░██║██╔══██║██║╚████║██║╚████║░░╚██╔╝░░  ██║╚██╔╝██║░░╚██╔╝░░░╚═══██╗╚██████╔╝██║
+    ██║░░░░░░░╚██╔╝░░╚█████╔╝╚█████╔╝██║░░██║██║░╚███║██║░╚███║░░░██║░░░  ██║░╚═╝░██║░░░██║░░░██████╔╝░╚═██╔═╝░███████╗
+    ╚═╝░░░░░░░░╚═╝░░░░╚════╝░░╚════╝░╚═╝░░╚═╝╚═╝░░╚══╝╚═╝░░╚══╝░░░╚═╝░░░  ╚═╝░░░░░╚═╝░░░╚═╝░░░╚═════╝░░░░╚═╝░░░╚══════╝
+
+    ░█████╗░░█████╗░░█████╗░███████╗░██████╗░██████╗  ██╗░░░░░░█████╗░██╗░░░██╗███████╗██████╗░
+    ██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝  ██║░░░░░██╔══██╗╚██╗░██╔╝██╔════╝██╔══██╗
+    ███████║██║░░╚═╝██║░░╚═╝█████╗░░╚█████╗░╚█████╗░  ██║░░░░░███████║░╚████╔╝░█████╗░░██████╔╝
+    ██╔══██║██║░░██╗██║░░██╗██╔══╝░░░╚═══██╗░╚═══██╗  ██║░░░░░██╔══██║░░╚██╔╝░░██╔══╝░░██╔══██╗
+    ██║░░██║╚█████╔╝╚█████╔╝███████╗██████╔╝██████╔╝  ███████╗██║░░██║░░░██║░░░███████╗██║░░██║
+    ╚═╝░░╚═╝░╚════╝░░╚════╝░╚══════╝╚═════╝░╚═════╝░  ╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚══════╝╚═╝░░╚═╝
+
+    [FVJOHNNY_MYSQL] Made with love by FVJohnny http://steamcommunity.com/id/FVJohnny/]]) 
 end
  
 FVJOHNNY_MYSQL:LoadFiles()
